9d999844caf3ef7c2374503784125392a87ae435
Antonio <antoniojosedonishung@gmail.com>
2023-10-03 12:53:37 -0500

    feat: implemented ingress for the gateway service

---
+++ modules/k8s-gateway/ingress.tf
@@ -0,0 +1,50 @@
+
+resource "kubernetes_manifest" "stripprefix_middleware" {
+  manifest = {
+    "apiVersion" = "traefik.containo.us/v1alpha1"
+    "kind"       = "Middleware"
+    "metadata" = {
+      "name"      = "stripprefix"
+      "namespace" = var.kube_namespace
+    }
+    "spec" = {
+      "stripPrefix" = {
+        "prefixes"   = ["/gw"]
+        "forceSlash" = false
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "gateway_ingress" {
+  metadata {
+    name      = "gateway-ingress"
+    namespace = var.kube_namespace
+
+    annotations = {
+      "traefik.ingress.kubernetes.io/router.middlewares" = "${var.kube_namespace}-stripprefix@kubernetescrd"
+      "kubernetes.io/ingress.class"                      = "traefik"
+    }
+  }
+
+  spec {
+    ingress_class_name = "traefik"
+    rule {
+      http {
+        path {
+          backend {
+            service {
+              name   = kubernetes_service.gateway_service.metadata[0].name
+              port {
+                number = 8080
+              }
+            }
+          }
+
+          path      = "/gw"
+          path_type = "Prefix"
+        }
+      }
+    }
+  }
+}
